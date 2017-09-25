4eeefaa82a1089cea7d931eae977541cba00e962
Floreit <Floreit@users.noreply.github.com>
2017-09-24 21:54:51 -0500

    Add files via upload

--- KyleLud 4.2.py
+++ KyleLud 4.2.py
@@ -1,3 +1,4 @@
+#delcaring variables and input
 days = int(input("How many days will you work for pennies a day?"))
 
 print("Days Worked    |  Amount  Earned  That  Day")
@@ -5,20 +6,21 @@ print("------------------------------------------")
 count = 1
 
 pay = 0.01
-
+#useless but kept it due to experimentation and not wanting to break things
 str = "     |"
 # 3 spaces for number 10 spaces for |
 while count <= days:
 
 
     day = count
+    #           This thing here, is called formatting, keeps the numbers going left and aligned perfectly
     print("   ",f'{day:>5}' , str , "$ " , f'{pay:.2f}' )
 
 
     pay = pay + pay
 
 
-    count = count + 1
+    count = count + 1 #stops the loop when reaching input days
 
 print(" ")
 print("Total pay is: " , pay)
