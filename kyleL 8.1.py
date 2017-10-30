8d2f43b04ee6ed64465d3299151c3a31eebe60ec
Floreit <Floreit@users.noreply.github.com>
2017-10-30 18:56:02 -0500

    Add files via upload

--- kyleL 8.1.py
+++ kyleL 8.1.py
@@ -1,10 +1,13 @@
+# input for user enterying there name
 name = input('Type your name and press ENTER. ')
+# split the input
 name_list = name.split()
 
-print(name_list)
 
+print(name_list)
+# separate the initials
 first = name_list[0][0]
 second = name_list[1][0]
 last = name_list[2][0]
-
+# print out the initials for a user
 print(first.title(),'.', second.title(),'.', last.title(), sep=" ")
