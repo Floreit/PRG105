df5db151a16380b8324f6a56a244b2defa9db249
Floreit <Floreit@users.noreply.github.com>
2017-11-01 19:42:05 -0500

    Add files via upload

---
+++ kyle inclass.py
@@ -0,0 +1,26 @@
+import random
+class Coin:
+    def __init__(self):
+        self.__side_up = 'heads'
+
+
+    def toss(self):
+        if random.randint(0,1) == 0:
+            self.__side_up = "heads"
+        else:
+            self.__side_up = 'tales'
+
+    def get_side_up(self):
+        return self.__side_up
+
+coin = Coin()
+heads = 0
+tails = 0
+for x in range(50):
+    coin.toss()
+    if coin.get_side_up() == "heads":
+        heads = heads + 1
+    else:
+        tails = tails + 1
+
+print("The number of times tails has been called: ", tails,"The number of times heads has been called is: ", heads)
